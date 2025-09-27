7721481d57950f66b14f7e1f3836bfc9cfc8a3e2
Rdunc7 <robld2002@gmail.com>
2025-09-27 03:11:44 -0400

    general.js changes 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,15 +13,23 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let ISBN = req.params.isbn;
+    const book = book[ISBN];
+
+    if(book) {
+        res.send(book)
+    } else {
+        res.status(404).json({"No book found"})
+    }
   
+ });
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
